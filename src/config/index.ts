bb734201d51791c5c719dc8f4fe0fbd549f7cc9e
jekabskarklins <jekabs.karklins@ess.eu>
2022-06-15 17:31:08 +0200

    feat: Dockerizing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,11 @@
 import 'reflect-metadata';
 
 switch (process.env.DEPENDENCY_CONFIG) {
-  case 'run':
-    require('./dependencyConfigRun');
-    break;
   case 'test':
     require('./dependencyConfigTest');
     break;
   default:
-    throw new Error(
-      `process.env.DEPENDENCY_CONFIG contains invalid value '${process.env.DEPENDENCY_CONFIG}'.
-       Available values are <run|test>`
-    );
+    require('./dependencyConfigRun');
 }
 
 export {};
