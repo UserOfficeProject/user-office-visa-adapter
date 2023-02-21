4cc3f06efb5c3cc1a70beed8ad37efb10c5dd6f5
Jekabs Karklins <Jekabs.Karklins@ess.eu>
2023-02-21 13:54:44 +0100

    Moving queue name to param

--- src/queue/consumers/rabbitMQ/RabbitMQConsumer.ts
+++ src/queue/consumers/rabbitMQ/RabbitMQConsumer.ts
@@ -6,9 +6,30 @@ import {
 
 import { QueueConsumer } from '../QueueConsumer';
 
+function validateEnv(): void {
+  if (!process.env.RABBITMQ_HOSTNAME) {
+    throw new Error('Environmental variable RABBITMQ_HOSTNAME not defined');
+  }
+
+  if (!process.env.RABBITMQ_USERNAME) {
+    throw new Error('Environmental variable RABBITMQ_USERNAME not defined');
+  }
+
+  if (!process.env.RABBITMQ_PASSWORD) {
+    throw new Error('Environmental variable RABBITMQ_PASSWORD not defined');
+  }
+
+  if (!process.env.VISA_QUEUE_NAME) {
+    throw new Error('Environmental variable VISA_QUEUE_NAME not defined');
+  }
+}
 export class RabbitMQConsumer implements QueueConsumer {
   private consumerCallback?: ConsumerCallback;
   private rabbitMQ: RabbitMQMessageBroker;
+
+  constructor() {
+    validateEnv();
+  }
 
   start(consumerCallback: ConsumerCallback): void {
     this.consumerCallback = consumerCallback;
@@ -21,7 +42,10 @@ export class RabbitMQConsumer implements QueueConsumer {
       port: Number(process.env.RABBITMQ_PORT) || 5672,
     });
 
-    this.rabbitMQ.listenOn(Queue.VISA, this.consumerCallback);
+    this.rabbitMQ.listenOn(
+      process.env.VISA_QUEUE_NAME as Queue,
+      this.consumerCallback
+    );
   }
 
   isReady() {
